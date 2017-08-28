3d4d4f72b0e629fa67b7e1bfd7256ff0d7ddd7db
liudian <hardy.liu@outlook.com>
2017-08-28 20:29:44 +0800

    add: 添加中文语言

---
+++ resources/lang/zh-CN/auth.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liudian
+ * Date: 6/8/17
+ * Time: 20:22
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed'   => '用户名或密码错误。',
+    'throttle' => '您的尝试登录次数过多. 请 :seconds 秒后再试。',
+    'failedAdmin' => '非管理员或用户名密码错误。'
+];
---
+++ resources/lang/zh-CN/pagination.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liudian
+ * Date: 6/8/17
+ * Time: 20:23
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+    'previous' => '&laquo; 上一页',
+    'next'     => '下一页 &raquo;',
+];
