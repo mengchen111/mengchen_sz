ad15b7f50e3b9ded63696edf88731fe6dfe58a27
liudian <hardy.liu@outlook.com>
2017-08-29 21:43:37 +0800

    update: 更新.gitignore文件

---
+++ app/Http/Controllers/Agent/TopUpController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Agent;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\TopUpAdmin;
+use App\Models\TopUpAgent;
+use App\Models\TopUpPlayer;
+use Illuminate\Support\Facades\Validator;
+use App\Models\OperationLogs;
+use Illuminate\Support\Facades\DB;
+
+class TopUpController
+{
+
+}
