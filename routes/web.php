3c824f21878208f9751d8b11692d38fc96db8089
liudian <hardy.liu@outlook.com>
2017-08-31 10:30:28 +0800

    update: 更新接口规范

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,17 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('list-session', 'DevToolsController@listSession');
-Route::get('hashed-pass/{pass}', 'DevToolsController@hashedPass');
-Route::get('info', 'InfoController@info');
+//开发调试功能接口
+Route::prefix('dev')->group(function () {
+    Route::get('list-session', 'DevToolsController@listSession');
+    Route::get('hashed-pass/{pass}', 'DevToolsController@hashedPass');
+
+});
+
+//公共ajax接口
+Route::prefix('api')->group(function () {
+    Route::get('info', 'InfoController@info');
+});
 
 //ajax接口
 Route::group([
@@ -50,7 +58,7 @@ Route::group([
 
 Route::group([
     'middleware' => ['auth'],
-    'prefix' => 'agent',
+    'prefix' => 'agent/api',
     'namespace' => 'Agent'
 ], function () {
     Route::get('subagent', 'SubAgentController@show');
@@ -62,4 +70,12 @@ Route::group([
     //Route::post('top-up/player/{receiver}/{amount}', 'TopUpController@topUp2Player')->where('amount', '[0-9]+');
     Route::get('top-up/child', 'TopUpController@topUp2ChildHistory');
     Route::get('top-up/player', 'TopUpController@topUp2PlayerHistory');
+});
+
+Route::group([
+    'middleware' => ['auth'],
+    'prefix' => 'agent',
+    'namespace' => 'Agent'
+], function () {
+    //Route::get('agent/list', 'ViewController@agentList');
 });
