9fd5bef3af0c31f2ae0856c82533ac602724dbef
liudian <hardy.liu@outlook.com>
2017-08-31 18:57:24 +0800

    add: 完成代理商充卡记录查询页面

--- app/Http/Controllers/Admin/TopUpController.php
+++ app/Http/Controllers/Admin/TopUpController.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\DB;
 
 class TopUpController extends Controller
 {
+    protected $per_page = 15;
+    protected $order = ['id', 'desc'];
+
+    public function __construct(Request $request)
+    {
+        $this->per_page = $request->per_page ?: $this->per_page;
+        $this->order = $request->sort ? explode('|', $request->sort) : $this->order;
+    }
+
     //给总代理商充卡
     public function topUp2TopAgent(Request $request, $receiver, $type, $amount)
     {
@@ -72,14 +81,11 @@ class TopUpController extends Controller
         });
     }
 
-    //管理员给总代的充卡记录
+    //管理员给总代的充值记录
     public function topUp2TopAgentHistory(Request $request)
     {
-        $per_page = $request->per_page ?: 10;
-        $order = $request->sort ? explode('|', $request->sort) : ['id', 'desc'];
-
         OperationLogs::insert(session('user')->id, $request->path(), $request->method(),
-            '总代理商充卡记录', json_encode($request->all()));
+            '总代理商充值记录', json_encode($request->all()));
 
         //搜索代理商
         if ($request->has('filter')) {
@@ -89,23 +95,40 @@ class TopUpController extends Controller
             }
             return  TopUpAdmin::with(['provider', 'receiver', 'item'])
                 ->whereIn('receiver_id', $receivers)
-                ->orderBy($order[0], $order[1])
-                ->paginate($per_page);
+                ->orderBy($this->order[0], $this->order[1])
+                ->paginate($this->per_page);
         }
 
         return TopUpAdmin::with(['provider', 'receiver', 'item'])
-            ->orderBy($order[0], $order[1])
-            ->paginate($per_page);
+            ->orderBy($this->order[0], $this->order[1])
+            ->paginate($this->per_page);
     }
 
-    //上级代理商给下级的充卡记录
-    public function Agent2AgentHistory()
+    //上级代理商给下级的充值记录
+    public function Agent2AgentHistory(Request $request)
     {
-        //TODO 日志记录
-        return TopUpAgent::with(['provider', 'receiver', 'item'])->get();
+        OperationLogs::insert(session('user')->id, $request->path(), $request->method(),
+            '代理商给代理商充值记录', json_encode($request->all()));
+
+        //搜索代理商，查找字符串包括发放者和接收者
+        if ($request->has('filter')) {
+            $accounts = array_column(User::where('account', 'like', "%{$request->filter}%")->get()->toArray(), 'id');
+            if (empty($accounts)) {
+                return null;
+            }
+            return TopUpAgent::with(['provider', 'receiver', 'item'])
+                ->whereIn('provider_id', $accounts)
+                ->whereIn('receiver_id', $accounts, 'or')
+                ->orderBy($this->order[0], $this->order[1])
+                ->paginate($this->per_page);
+        }
+
+        return TopUpAgent::with(['provider', 'receiver', 'item'])
+            ->orderBy($this->order[0], $this->order[1])
+            ->paginate($this->per_page);
     }
 
-    //代理商给玩家的充卡记录
+    //代理商给玩家的充值记录
     public function Agent2PlayerHistory()
     {
         //TODO 日志记录
