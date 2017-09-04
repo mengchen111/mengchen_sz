395e01d42f55f93ace95be172a8ba7646b9f0507
liudian <hardy.liu@outlook.com>
2017-09-04 14:33:59 +0800

    add: 更改inventory的migrate，添加管理员的金币初始库存。给main-header上面展示库存数量

--- database/migrations/2017_08_30_110851_create_inventory_table.php
+++ database/migrations/2017_08_30_110851_create_inventory_table.php
@@ -8,10 +8,18 @@ use Illuminate\Support\Facades\DB;
 class CreateInventoryTable extends Migration
 {
     protected $adminInitStock = [
-        'id' => 1,
-        'user_id' => 1,
-        'item_id' => 1, //1房卡，2金币
-        'stock' => 10000,
+        [
+            'id' => 1,
+            'user_id' => 1,
+            'item_id' => 1, //1房卡，2金币
+            'stock' => 10000,
+        ],
+        [
+            'id' => 2,
+            'user_id' => 1,
+            'item_id' => 2, //1房卡，2金币
+            'stock' => 10000,
+        ]
     ];
 
     /**
