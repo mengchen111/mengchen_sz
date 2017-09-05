983744097b079f8b03a0fe6430f4595b901b179e
liudian <hardy.liu@outlook.com>
2017-09-05 20:50:11 +0800

    update: 删除inventory的管理员初始库存的添加，申请库存即可

--- database/migrations/2017_08_30_110851_create_inventory_table.php
+++ database/migrations/2017_08_30_110851_create_inventory_table.php
@@ -7,21 +7,6 @@ use Illuminate\Support\Facades\DB;
 
 class CreateInventoryTable extends Migration
 {
-    protected $adminInitStock = [
-        [
-            'id' => 1,
-            'user_id' => 1,
-            'item_id' => 1, //1房卡，2金币
-            'stock' => 10000,
-        ],
-        [
-            'id' => 2,
-            'user_id' => 1,
-            'item_id' => 2, //1房卡，2金币
-            'stock' => 10000,
-        ]
-    ];
-
     /**
      * Run the migrations.
      *
@@ -36,8 +21,6 @@ class CreateInventoryTable extends Migration
             $table->unsignedInteger('stock')->comment('库存');
             $table->timestamps();
         });
-
-        DB::table('inventory')->insert($this->adminInitStock);
     }
 
     /**
