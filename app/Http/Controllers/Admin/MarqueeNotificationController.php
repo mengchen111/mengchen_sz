6ec2e4d387d6df15067bf4a6d15c2b06b18ce59a
liudian <hardy.liu@outlook.com>
2017-09-08 18:10:51 +0800

    fix: 修复跑马灯公告同步问题（没有将数据库的时间转换为时间戳）

--- app/Http/Controllers/Admin/MarqueeNotificationController.php
+++ app/Http/Controllers/Admin/MarqueeNotificationController.php
@@ -11,20 +11,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Jobs\SendMarqueeNotification;
-use GuzzleHttp\Exception\ConnectException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Models\GameNotificationMarquee;
 use App\Models\OperationLogs;
 use App\Exceptions\CustomException;
-use GuzzleHttp\Client;
+use Carbon\Carbon;
 
 class MarqueeNotificationController extends Controller
 {
     protected $per_page = 15;
     protected $order = ['id', 'desc'];
 
+    protected $dateFormat = 'Y-m-d H:i:s';  //日期时间格式
     protected $apiAddress = '';     //游戏服开放的接口的url地址
     protected $formData = [         //游戏服接口必须要填充的数据，不然报错
         'title' => '',
@@ -120,8 +120,8 @@ class MarqueeNotificationController extends Controller
         $this->formData['id'] = $data['id'];
         $this->formData['level'] = $data['priority'];
         $this->formData['diff_time'] = $data['interval'];
-        $this->formData['stime'] = $data['start_at'];
-        $this->formData['etime'] = $data['end_at'];
+        $this->formData['stime'] = Carbon::createFromFormat($this->dateFormat, $data['start_at'])->timestamp;
+        $this->formData['etime'] = Carbon::createFromFormat($this->dateFormat, $data['end_at'])->timestamp;
         $this->formData['content'] = $data['content'];
     }
 
