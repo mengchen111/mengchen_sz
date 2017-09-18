98663dc098fea97c6ccf34ce635e093a2d7fe253
liudian <hardy.liu@outlook.com>
2017-09-18 18:54:36 +0800

    update: 删除client目录下的dist和build目录(暂时不需要)

--- client/build/build.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const webpackConfig = require('../webpack.config.js')
-const webpack = require('webpack')
-
-webpack(webpackConfig, function (err, stat) {
-    if (err) throw err;
-});
