d1a09c3dda9e746b5d1ce30df31ca2701e426a7f
liudian <hardy.liu@outlook.com>
2017-09-23 14:29:53 +0800

    update: 将/路由移到Home控制器下

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return redirect('/login');
-});
+Route::get('/', 'HomeController@index');
 
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
