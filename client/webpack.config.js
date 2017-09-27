0d83632c1d6a7a00261e4a003eb155d1efdf618d
liudian <hardy.liu@outlook.com>
2017-09-27 20:16:13 +0800

    update: 更新babel的preset为env

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,7 @@ const config = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['es2015', 'stage-0']
+              presets: ['env']
             },
           },
           {
