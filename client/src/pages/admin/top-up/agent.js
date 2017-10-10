af23e4402d5775e88adf67e9e6ff212508570af8
liudian <hardy.liu@outlook.com>
2017-10-10 18:25:53 +0800

    update: 完成充值记录板块的js和html代码分离

---
+++ client/src/pages/admin/top-up/agent.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import MyVuetable from '../../../components/MyVuetable.vue'
+import FilterBar from '../../../components/FilterBar.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    FilterBar,
+    MyVuetable,
+  },
+  data: {
+    eventHub: new Vue(),
+    tableUrl: '/admin/api/top-up/agent',
+    tableFields: [
+      {
+        name: 'id',
+        title: 'ID',
+        sortField: 'id',
+      },
+      {
+        name: 'provider.account',
+        title: '发放者(上级代理)',
+        sortField: 'provider_id',
+      },
+      {
+        name: 'receiver.account',
+        title: '接收者(下级代理)',
+        sortField: 'receiver_id',
+      },
+      {
+        name: 'item.name',
+        title: '充值类型',
+        sortField: 'type',
+      },
+      {
+        name: 'amount',
+        title: '充值数量',
+        sortField: 'amount',
+      },
+      {
+        name: 'created_at',
+        title: '充值时间',
+      },
+    ],
+  },
+
+})
---
+++ client/src/pages/admin/top-up/player.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import MyVuetable from '../../../components/MyVuetable.vue'
+import FilterBar from '../../../components/FilterBar.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    FilterBar,
+    MyVuetable,
+  },
+  data: {
+    eventHub: new Vue(),
+    tableUrl: '/admin/api/top-up/player',
+    tableFields: [
+      {
+        name: 'id',
+        title: 'ID',
+        sortField: 'id',
+      },
+      {
+        name: 'provider.account',
+        title: '发放者(代理商)',
+        sortField: 'provider_id',
+      },
+      {
+        name: 'player',
+        title: '玩家',
+        sortField: 'player',
+      },
+      {
+        name: 'item.name',
+        title: '充值类型',
+        sortField: 'type',
+      },
+      {
+        name: 'amount',
+        title: '充值数量',
+        sortField: 'amount',
+      },
+      {
+        name: 'created_at',
+        title: '充值时间',
+      },
+    ],
+  },
+
+})
