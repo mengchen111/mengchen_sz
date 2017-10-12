e2ed2cdff6a3ad2a76bec5a3261332fc715a8e1f
liudian <hardy.liu@outlook.com>
2017-10-12 10:58:10 +0800

    update: 清理不需要的控制器,路由条目,migrate和model

--- routes/web.php
+++ routes/web.php
@@ -39,31 +39,8 @@ Route::group([
     Route::put('self/password', 'AdminController@updatePass');
 
 //    Route::get('home/summary', 'HomeController@summaryReport');
-//
-//    Route::get('statement/hourly', 'StatementController@hourly');
-//    Route::get('statement/daily', 'StatementController@daily');
-//    Route::get('statement/monthly', 'StatementController@monthly');
-//    Route::get('statement/hourly-chart', 'StatementController@hourlyChart');
-//
+
     Route::get('game/player', 'Game\PlayerController@show');
-//
-//    Route::get('game/notification/marquee', 'Game\MarqueeNotificationController@show');
-//    Route::post('game/notification/marquee', 'Game\MarqueeNotificationController@create');
-//    Route::put('game/notification/marquee/{marquee}', 'Game\MarqueeNotificationController@update')->where('marquee', '[0-9]+');
-//    Route::delete('game/notification/marquee/{marquee}', 'Game\MarqueeNotificationController@destroy')->where('marquee', '[0-9]+');
-//    Route::put('game/notification/marquee/enable/{marquee}', 'Game\MarqueeNotificationController@enable')->where('marquee', '[0-9]+');
-//    Route::put('game/notification/marquee/disable/{marquee}', 'Game\MarqueeNotificationController@disable')->where('marquee', '[0-9]+');
-//    Route::get('game/notification/login', 'Game\LoginNotificationController@show');
-//    Route::post('game/notification/login', 'Game\LoginNotificationController@create');
-//    Route::put('game/notification/login/{notification}', 'Game\LoginNotificationController@update')->where('notification', '[0-9]+');
-//    Route::delete('game/notification/login/{notification}', 'Game\LoginNotificationController@destroy')->where('notification', '[0-9]+');
-//    Route::put('game/notification/login/enable/{notification}', 'Game\LoginNotificationController@enable')->where('notification', '[0-9]+');
-//    Route::put('game/notification/login/disable/{notification}', 'Game\LoginNotificationController@disable')->where('notification', '[0-9]+');
-//
-//    Route::get('game/room/friend', 'Game\FriendRoomController@show');
-//    Route::delete('game/room/friend/{ownerId}', 'Game\FriendRoomController@dismiss')->where('ownerId', '[0-9]+');
-//    Route::get('game/room/coin', 'Game\CoinRoomController@show');
-//    Route::delete('game/room/coin/{roomId}', 'Game\CoinRoomController@dismiss')->where('roomId', '[0-9]+');
 
     Route::post('stock', 'StockController@apply');
     Route::get('stock/list', 'StockController@applyList');
@@ -94,16 +71,7 @@ Route::group([
 ], function () {
     Route::get('home', 'ViewController@home');
 
-//    Route::get('statement/hourly', 'ViewController@statementHourly');
-//    Route::get('statement/daily', 'ViewController@statementDaily');
-//    Route::get('statement/monthly', 'ViewController@statementMonthly');
-
     Route::get('player/list', 'ViewController@playerList');
-
-//    Route::get('gm/notification/marquee', 'ViewController@gmNotificationMarquee');
-//    Route::get('gm/notification/login', 'ViewController@gmNotificationLogin');
-//    Route::get('gm/room/friend', 'ViewController@gmRoomFriend');
-//    Route::get('gm/room/coin', 'ViewController@gmRoomCoin');
 
     Route::get('stock/apply-request', 'ViewController@stockApplyRequest');
     Route::get('stock/apply-list', 'ViewController@stockApplyList');
