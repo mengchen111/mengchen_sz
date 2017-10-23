2926d3828f6745ba647b5256818aae6691d592bc
liudian <hardy.liu@outlook.com>
2017-10-24 00:00:15 +0800

    update: 使用css media选择器来实现手机模式下的table的滚动条添加

--- client/src/pages/admin/agent/list.js
+++ client/src/pages/admin/agent/list.js
@@ -42,7 +42,6 @@ new Vue({
     editApiPrefix: '/admin/api/agent',
     updatePassApiPrefix: '/admin/api/agent/pass',
     deleteApiPrefix: '/admin/api/agent',
-    tableClass: 'row',
 
     tableUrl: '/admin/api/agent',
     tableFields: [
@@ -195,12 +194,6 @@ new Vue({
 
   mounted: function () {
     let _self = this
-
-    //判断屏幕大小，更新div的class，使table在手机浏览器下带上滚动条
-    let windowWidth = document.body.clientWidth
-    if (windowWidth < 768) {
-      this.tableClass = 'row pre-scrollable'
-    }
 
     this.$root.eventHub.$on('topUpAgentEvent', (data) => _self.activatedRow = data)
     this.$root.eventHub.$on('editInfoEvent', (data) => _self.activatedRow = data)
--- client/src/pages/admin/player/list.js
+++ client/src/pages/admin/player/list.js
@@ -24,7 +24,6 @@ new Vue({
       typeId: 1,
       amount: null,
     },
-    tableClass: 'row',  //绑定class，让table在手机浏览器模式下切换为滚动条
 
     tableUrl: '/admin/api/game/players',
     tableTrackBy: 'id',
@@ -105,12 +104,6 @@ new Vue({
 
   mounted: function () {
     let _self = this
-
-    //判断屏幕大小，更新div的class，使table在手机浏览器下带上滚动条
-    let windowWidth = document.body.clientWidth
-    if (windowWidth < 768) {
-      this.tableClass = 'row pre-scrollable'
-    }
 
     this.$root.eventHub.$on('topUpPlayerEvent', (data) => _self.activatedRow = data)
     this.$root.eventHub.$on('vuetableDataError', (data) => alert(data.error))
