2c523eb75422061db47c5e9af55a3d404d2ca8b2
liudian <hardy.liu@outlook.com>
2017-11-04 12:07:04 +0800

    add: 添加radio和checkbox插件

--- client/src/pages/admin/gm/room.js
+++ client/src/pages/admin/gm/room.js
@@ -1,2 +1,29 @@
 import '../common.js'
 import MyToastr from '../../../components/MyToastr.vue'
+import {Checkbox, Radio} from 'vue-checkbox-radio'
+
+new Vue({
+  el: '#app',
+  components: {
+    Checkbox,
+    Radio,
+    MyToastr,
+  },
+  data: {
+
+  },
+
+  methods: {
+    displayOpenRoom () {
+      console.log('display open room')
+    },
+
+    displayRoomHistory () {
+      console.log('display room history')
+    },
+
+    createRoom () {
+      console.log('create room')
+    }
+  },
+})
