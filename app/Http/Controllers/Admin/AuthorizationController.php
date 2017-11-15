405e5fd1c708e4446c8f480abc23d610ab41b591
liudian <hardy.liu@outlook.com>
2017-11-15 11:33:26 +0800

    add: 添加视图访问权限的接口文件（功能未完成）

---
+++ app/Http/Controllers/Admin/AuthorizationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\AdminRequest;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Admin\AuthorizationMap;
+
+class AuthorizationController extends Controller
+{
+    use AuthorizationMap;
+
+    public function showViewAccess(AdminRequest $request)
+    {
+        //TODO
+    }
+}
