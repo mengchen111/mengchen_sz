56d421a3e27dcfab2cbfa55d8179cda8c9a64489
liudian <hardy.liu@outlook.com>
2017-11-23 10:23:30 +0800

    add：添加myTools工具

--- client/src/pages/admin/common.js
+++ client/src/pages/admin/common.js
@@ -1,2 +1,4 @@
 import '../main-header.js'
 import './sidebar.js'
+
+export { myTools } from '../../tools/myTools.js'
---
+++ client/src/tools/myTools.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+let myTools = {
+  //axios默认实例
+  axiosInstance: axios.create({
+    timeout: 5000,                          //超时时间
+    xsrfCookieName: 'XSRF-TOKEN',
+    xsrfHeaderName: 'X-XSRF-TOKEN',
+    validateStatus: function (status) {
+      return status === 200 || status === 422
+    },
+  }),
+
+  //处理laravel返回的消息，并用toastr提示
+  msgResolver: function (res, toastr) {
+    if (res.status === 422) {
+      return toastr.message(JSON.stringify(res.data), 'error')
+    }
+    if (res.data.error) {
+      return toastr.message(res.data.error, 'error')
+    }
+    return toastr.message(res.data.message)
+  },
+}
+
+export { myTools }
+export default myTools
