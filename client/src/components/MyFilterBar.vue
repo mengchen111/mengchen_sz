0a4a2d4edd6074a0361ed285c513679fd80c225c
liudian <hardy.liu@outlook.com>
2017-11-27 16:48:54 +0800

    update: 更新FilterBar组件，优化玩家列表页面的手机显示效果

--- client/src/components/MyFilterBar.vue
+++ client/src/components/MyFilterBar.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="row" style="margin-right: 50px;margin-bottom: 8px;">
-        <div class="input-group col-xs-4 pull-right">
-            <input type="text" v-model.trim="filterText" class="form-control" @keyup.enter="doFilter"
-                   :placeholder="placeholder">
-            <span class="input-group-btn">
-                <button class="btn btn-flat" @click="doFilter" style="background-color: #b5bbc8">
-                    <i class="glyphicon glyphicon-search"></i>
-                </button>
-            </span>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="input-group col-md-4 col-xs-6" :class="filterBarClass" :style="css">
+                <input type="text" v-model.trim="filterText" class="form-control" @keyup.enter="doFilter"
+                       :placeholder="placeholder">
+                <span class="input-group-btn">
+                    <button class="btn btn-flat" @click="doFilter" style="background-color: #b5bbc8">
+                        <i class="glyphicon glyphicon-search"></i>
+                    </button>
+                </span>
+            </div>
         </div>
     </div>
 </template>
@@ -18,6 +20,14 @@
       placeholder: {
         type: String,
         default: '',
+      },
+      filterBarClass: {
+        required: false,
+        default: 'pull-right',
+      },
+      css: {
+        required: false,
+        default: 'margin-bottom: 10px;',
       },
     },
     data: function () {
