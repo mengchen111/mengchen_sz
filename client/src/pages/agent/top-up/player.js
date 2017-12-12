5967cea014a4ce15f565158fa564813e251e4bc4
liudian <hardy.liu@outlook.com>
2017-12-12 15:33:59 +0800

    update: 优化充值页面的手机端显示效果

--- client/src/pages/agent/top-up/player.js
+++ client/src/pages/agent/top-up/player.js
@@ -12,34 +12,34 @@ new Vue({
     eventHub: new Vue(),
     tableUrl: '/agent/api/top-up/player',
     tableFields: [
-      {
-        name: 'id',
-        title: 'ID',
-        sortField: 'id',
-      },
-      {
-        name: 'provider.account',
-        title: '发放者(此代理)',
-        sortField: 'provider_id',
-      },
+      // {
+      //   name: 'id',
+      //   title: 'ID',
+      //   sortField: 'id',
+      // },
+      // {
+      //   name: 'provider.account',
+      //   title: '发放者(此代理)',
+      //   sortField: 'provider_id',
+      // },
       {
         name: 'player',
-        title: '接收者(玩家)',
+        title: '玩家id',
         sortField: 'player',
       },
       {
         name: 'item.name',
-        title: '充值类型',
+        title: '类型',
         sortField: 'type',
       },
       {
         name: 'amount',
-        title: '充值数量',
+        title: '数量',
         sortField: 'amount',
       },
       {
         name: 'created_at',
-        title: '充值时间',
+        title: '时间',
       },
     ],
   },
