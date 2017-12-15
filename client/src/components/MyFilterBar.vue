4f4d4cfe25e07d05fca030d301b04e7228dbb978
liudian <hardy.liu@outlook.com>
2017-12-15 10:11:19 +0800

    update： 更新filterbar组件，同步更新管理员和代理商后台的页面

--- client/src/components/MyFilterBar.vue
+++ client/src/components/MyFilterBar.vue
@@ -1,16 +1,12 @@
 <template>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="input-group col-md-4 col-xs-6" :class="filterBarClass" :style="css">
-                <input type="text" v-model.trim="filterText" class="form-control" @keyup.enter="doFilter"
-                       :placeholder="placeholder">
-                <span class="input-group-btn">
-                    <button class="btn btn-flat" @click="doFilter" style="background-color: #b5bbc8">
-                        <i class="glyphicon glyphicon-search"></i>
-                    </button>
-                </span>
-            </div>
-        </div>
+    <div class="input-group" :class="filterBarClass" :style="css">
+        <input type="text" v-model.trim="filterText" class="form-control" @keyup.enter="doFilter"
+               :placeholder="placeholder">
+        <span class="input-group-btn">
+            <button class="btn btn-flat" @click="doFilter" style="background-color: #b5bbc8">
+                <i class="glyphicon glyphicon-search"></i>
+            </button>
+        </span>
     </div>
 </template>
 
