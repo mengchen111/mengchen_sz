3e3e429a706542346f862827468e2d987ae3d13e
liudian <hardy.liu@outlook.com>
2018-02-24 10:46:52 +0800

    update: 将summary.css中的样式代码移到custom.css中，使用class来替换之

--- client/src/pages/admin/activities/statement.js
+++ client/src/pages/admin/activities/statement.js
@@ -1,17 +1,24 @@
 import { myTools } from '../index.js'
+import MyDatePicker from '../../../components/MyDatePicker.vue'
+import MyToastr from '../../../components/MyToastr.vue'
 
 new Vue({
   el: '#app',
   components: {
-    //
+    MyDatePicker,
+    MyToastr,
   },
   data: {
     eventHub: new Vue(),
-    loading: true,
+    dateFormat: 'YYYY-MM-DD',
+    date: moment().format('YYYY-MM-DD'),
   },
 
   methods: {
-    //
+    changeDate () {
+      //todo 获取数据 双向绑定
+      console.log(this.date, 'change date')
+    },
   },
 
   created: function () {
@@ -19,6 +26,6 @@ new Vue({
   },
 
   mounted: function () {
-    console.log('statement')
+    //
   },
 })
