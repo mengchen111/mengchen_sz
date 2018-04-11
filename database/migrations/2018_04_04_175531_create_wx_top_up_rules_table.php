1223b933f17be353609e7efdb2642f75e1eb7743
YKAEJ <499627283@qq.com>
2018-04-11 11:39:00 +0800

    修改多余的migration 文件

--- database/migrations/2018_04_04_175531_create_wx_top_up_rules_table.php
+++ database/migrations/2018_04_04_175531_create_wx_top_up_rules_table.php
@@ -22,8 +22,48 @@ class CreateWxTopUpRulesTable extends Migration
             $table->string('remark')->nullable()->comment('备注');
             $table->timestamps();
         });
+        $this->initData();
     }
 
+    public function initData()
+    {
+        $time = \Carbon\Carbon::now()->toDateTimeString();
+        $data = [
+            [
+                'amount' => 100,
+                'give' => 0,
+                'first_give' => 50,
+                'price' => 80,
+                'remark' => '100张',
+                'created_at' => $time
+            ],
+            [
+                'amount' => 300,
+                'give' => 50,
+                'first_give' => 50,
+                'price' => 240,
+                'remark' => '300张+赠送50张',
+                'created_at' => $time
+            ],
+            [
+                'amount' => 1000,
+                'give' => 200,
+                'first_give' => 50,
+                'price' => 800,
+                'remark' => '1000张+赠送200张',
+                'created_at' => $time
+            ],
+            [
+                'amount' => 2000,
+                'give' => 500,
+                'first_give' => 50,
+                'price' => 1600,
+                'remark' => '2000张+赠送500张',
+                'created_at' => $time
+            ],
+        ];
+        DB::table('wx_top_up_rules')->insert($data);
+    }
     /**
      * Reverse the migrations.
      *
--- database/migrations/2018_04_04_181117_create_rebate_rules_table.php
+++ database/migrations/2018_04_04_181117_create_rebate_rules_table.php
@@ -20,8 +20,46 @@ class CreateRebateRulesTable extends Migration
             $table->string('remark')->nullable();
             $table->timestamps();
         });
+        $this->initData();
     }
 
+    public function initData()
+    {
+        $time = \Carbon\Carbon::now()->toDateTimeString();
+        $data = [
+            [
+                'price' => 3000,
+                'rate' => 5,
+                'remark' => '3000 返利5%',
+                'created_at' => $time
+            ],
+            [
+                'price' => 10000,
+                'rate' => 10,
+                'remark' => '10000 返利10%',
+                'created_at' => $time
+            ],
+            [
+                'price' => 30000,
+                'rate' => 15,
+                'remark' => '30000 返利15%',
+                'created_at' => $time
+            ],
+            [
+                'price' => 100000,
+                'rate' => 20,
+                'remark' => '100000 返利20%',
+                'created_at' => $time
+            ],
+            [
+                'price' => 250000,
+                'rate' => 25,
+                'remark' => '250000 返利25%',
+                'created_at' => $time
+            ],
+        ];
+        DB::table('rebate_rules')->insert($data);
+    }
     /**
      * Reverse the migrations.
      *
