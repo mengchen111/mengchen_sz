b08b8e0d37a7c543aeeb8126d6146fd6a3c94f03
liudian <hardy.liu@outlook.com>
2018-05-26 09:21:28 +0800

    update: 将swagger definitions用一个统一的的文件来引入

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -65,8 +65,7 @@ use Illuminate\Http\Request;
  *     ),
  * )
  */
-require app_path('Swagger/PlayerDefinition.php');   //载入玩家模型的swagger定义
-require app_path('Swagger/PaginationParamsDefinition.php'); //分页通用参数
+include app_path('Swagger/RequireDefinitions.php');
 
 class Controller extends BaseController
 {
---
+++ app/Swagger/RequireDefinitions.php
@@ -0,0 +1,4 @@
+<?php
+
+include app_path('Swagger/PlayerDefinition.php');   //载入玩家模型的swagger定义
+include app_path('Swagger/PaginationParamsDefinition.php'); //分页通用参数
